efb0ab9e9202c9925aa4e278394e734a3d830fb5
RHsanto <rakibulsanto789@gmail.com>
2023-09-01 17:16:07 +0600

    add some comments

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+// mongodb database
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mr-travel-app.aqkf7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
