297a9d5dfb6bda9d1d41f590f3e596b44eaa5d1f
RHsanto <rakibulsanto789@gmail.com>
2023-11-29 18:18:08 +0600

    edit

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const { getProducts, addProduct, payments } = require("../controllers/ProductsControllers");
+const {
+  getProducts,
+  addProduct,
+  payments,
+  deleteProduct,
+} = require("../controllers/ProductsControllers");
 const router = express.Router();
 
 // Here Products Route
 router.get("/products", getProducts);
 router.post("/product", addProduct);
 router.post("/create-checkout-session", payments);
-router.post("/products/:id", addProduct);
+router.post("/products/:id", deleteProduct);
 
 module.exports = router;
